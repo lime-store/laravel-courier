3e1708470eb9ad03e4595b1da0346ed32e569f89
Emil <emil.gasanovua@gmail.com>
2026-06-14 10:57:04 +0400

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,9 @@ class UserSeeder extends Seeder
     public function run(): void
     {
   
-
+        User::create(['name' => 'Admin', 'email' => 'admin@test.com', 'password' => bcrypt('password'), 'role' => 'admin']);
+        User::create(['name' => 'Courier1', 'email' => 'courier@test.com', 'password' => bcrypt('password'), 'role' => 'courier']);
+    
         // Создаём тестовые заказы
         Order::create(['address_from' => 'ул. Ленина, 1',   'address_to' => 'ул. Пушкина, 5']);
         Order::create(['address_from' => 'пр. Мира, 10',    'address_to' => 'ул. Гагарина, 3']);
