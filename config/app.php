5461fd73af39ffa722737ccc9a36c3ca20eedbe4
Emil <emil.gasanovua@gmail.com>
2026-06-15 11:07:47 +0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Baku',
+    'timezone' => env('APP_TIMEZONE', 'Asia/Baku'),
 
     /*
     |--------------------------------------------------------------------------
